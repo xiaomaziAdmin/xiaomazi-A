2975754860d85641ca3dd01ad20ea79799d3c916
xiaomazi <xiaomazi@bjpowernode.com>
2021-05-10 21:11:16 +0800

    test02

--- src/main/java/com/xiaomazi/springboot/model/Student.java
+++ src/main/java/com/xiaomazi/springboot/model/Student.java
@@ -1,11 +1,22 @@
 package com.xiaomazi.springboot.model;
 
 public class Student {
+
     private Integer id;
 
     private String name;
 
     private Integer age;
+
+    private String address;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 
     public Integer getId() {
         return id;
