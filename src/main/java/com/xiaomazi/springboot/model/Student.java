5eec6d4cdb667f3f67952cd18bdfa84042131c10
xiaomazi <xiaomazi@bjpowernode.com>
2021-05-10 21:42:20 +0800

    test04

--- src/main/java/com/xiaomazi/springboot/model/Student.java
+++ src/main/java/com/xiaomazi/springboot/model/Student.java
@@ -10,6 +10,17 @@ public class Student {
 
     private String address;
 
+    private String school;
+
+
+    public String getSchool() {
+        return school;
+    }
+
+    public void setSchool(String school) {
+        this.school = school;
+    }
+
     public String getAddress() {
         return address;
     }
